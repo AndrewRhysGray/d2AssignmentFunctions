e5e955838714fdea9fe0ef1e76ee85d8eb8f9755
Andrew Gray <andrew.rhysgray@hotmail.co.uk>
2024-02-07 16:08:55 +0000

    Corrected answers to the arrow function conversion assignments

---
+++ d2FunctionsAssignments.js
@@ -0,0 +1,23 @@
+let greet = name => {
+    return "Hi " + name + "!";
+}
+
+let isEven = num => {
+    return num % 2 === 0;
+}
+
+let counterFunc = counter => {
+    return counter > 100 ? 0 : counter + 1;
+}
+
+let nameAge = (name, age) => {
+    return "Hello " + name + ". You are " + age + " years old.";
+}
+
+let printOnly = () => {
+    console.log("printing");
+}
+
+let sum = (num1, num2) => {
+    return num1 + num2;
+}
